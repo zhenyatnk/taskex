9a2153b2484ce617f28f30df50aaf30fde6dd0c2
Zhenya_tnk <tinkalyk@mail.ru>
2020-03-24 23:27:35 +0300

    add CreateCollection and ContainTasks functions

--- core/include/taskex/core/taskex.h
+++ core/include/taskex/core/taskex.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <vector>
 #include <future>
 #include <functional>
 
@@ -9,75 +10,77 @@ template<typename RetType>
 class Task final
 {
 public:
-    explicit Task(const std::function<RetType(void)>& thenFunc)
+    template<typename TypeFunc>
+    explicit Task(TypeFunc&& thenFunc) noexcept
     {
-        m_future = std::async(std::launch::async, [=](){return thenFunc();});
+        m_future = std::async(std::launch::async, [_thenFunc = std::move(thenFunc)] () mutable {return _thenFunc();});
     }
     template<typename TaskParms>
-    Task(const std::function<RetType(void)>& thenFunc, std::future<TaskParms>&& params)
+    Task(std::function<RetType(void)> thenFunc, std::future<TaskParms>&& params) noexcept
     {
-        m_future = std::async(std::launch::async, [thenFunc, parm = std::move(params)]() mutable
+        m_future = std::async(std::launch::async, [_thenFunc = std::move(thenFunc), parm = std::move(params)]() mutable
         {
             parm.get();
-            return thenFunc();
+            return _thenFunc();
         });
     }
     template<typename TaskParms>
-    Task(const std::function<RetType(TaskParms)>& thenFunc, std::future<TaskParms>&& params)
+    Task(std::function<RetType(TaskParms)> thenFunc, std::future<TaskParms>&& params) noexcept
     {
-        m_future = std::async(std::launch::async, [thenFunc, parm = std::move(params)]() mutable
+        m_future = std::async(std::launch::async, [_thenFunc = std::move(thenFunc), parm = std::move(params)]() mutable
         {
-            return thenFunc(parm.get());
+            return _thenFunc(parm.get());
         });
     }
-    RetType get()
+    RetType get() const
     {
         if(m_future.valid())
-           return m_future.get();
+           m_value = m_future.get();
         return m_value;
     }
 
     template<typename RetThen>
-    auto then(const std::function<RetThen(RetType)>& thenFunc)
+    auto then(std::function<RetThen(RetType)> thenFunc)
     {
-        return Task<RetThen>(thenFunc, std::move(m_future));
+        return Task<RetThen>(std::move(thenFunc), std::move(m_future));
     }
     template<typename RetThen>
-    auto then(const std::function<RetThen(void)>& thenFunc)
+    auto then(std::function<RetThen(void)> thenFunc)
     {
-        return Task<RetThen>(thenFunc, std::move(m_future));
+        return Task<RetThen>(std::move(thenFunc), std::move(m_future));
     }
     
 protected:
-    std::future<RetType> m_future;
-    RetType m_value = {};
+    mutable std::future<RetType> m_future;
+    mutable RetType m_value = {};
 };
 template<>
 class Task<void> final
 {
 public:
-    explicit Task(const std::function<void(void)>& thenFunc)
+    template<typename TypeFunc>
+    explicit Task(TypeFunc&& thenFunc) noexcept
     {
-        m_future = std::async(std::launch::async, [=](){return thenFunc();});
+        m_future = std::async(std::launch::async, [_thenFunc = std::move(thenFunc)] () mutable {return _thenFunc();});
     }
     template<typename TaskParms>
-    Task(const std::function<void(void)>& thenFunc, std::future<TaskParms>&& params)
+    Task(std::function<void(void)> thenFunc, std::future<TaskParms>&& params) noexcept
     {
-        m_future = std::async(std::launch::async, [thenFunc, parm = std::move(params)]() mutable
+        m_future = std::async(std::launch::async, [_thenFunc = std::move(thenFunc), parm = std::move(params)]() mutable
         {
             parm.get();
-            return thenFunc();
+            return _thenFunc();
         });
     }
     template<typename TaskParms>
-    Task(const std::function<void(TaskParms)>& thenFunc, std::future<TaskParms>&& params)
+    Task(std::function<void(TaskParms)> thenFunc, std::future<TaskParms>&& params) noexcept
     {
-        m_future = std::async(std::launch::async, [thenFunc, parm = std::move(params)]() mutable
+        m_future = std::async(std::launch::async, [_thenFunc = std::move(thenFunc), parm = std::move(params)]() mutable
         {
-            return thenFunc(parm.get());
+            return _thenFunc(parm.get());
         });
     }
-    void get()
+    void get() const
     {
         if(m_future.valid())
             m_future.get();
@@ -90,7 +93,47 @@ public:
     }
     
 protected:
-    std::future<void> m_future;
+    mutable std::future<void> m_future;
 };
+
+template<typename RetType, typename ParamType>
+std::vector<Task<RetType>> CreateCollection(const std::function<RetType(ParamType)>& func, const std::vector<ParamType>& params)
+{
+    std::vector<Task<RetType>> ret;
+    ret.reserve(params.size());
+    for(const auto& param: params)
+        ret.push_back(Task<RetType>([param, func](){ return func(param);}));
+    return ret;
+}
+template<typename ParamType>
+std::vector<Task<void>> CreateCollection(const std::function<void(ParamType)>& func, const std::vector<ParamType>& params)
+{
+    std::vector<Task<void>> ret;
+    ret.reserve(params.size());
+    for(const auto& param: params)
+        ret.push_back(Task<void>([param, func](){ func(param);}));
+    return ret;
+}
+
+template<typename RetType>
+Task<std::vector<RetType>> ContainTasks(std::vector<Task<RetType>>&& tasks)
+{
+    return Task<std::vector<RetType>>{[_tasks = std::move(tasks)] () mutable
+        {
+            std::vector<RetType> ret;
+            ret.reserve(_tasks.size());
+            for(const auto& task: _tasks)
+                ret.push_back(task.get());
+            return ret;
+        }};
+}
+Task<void> ContainTasks(std::vector<Task<void>>&& tasks)
+{
+    return Task<void>{[_tasks = std::move(tasks)] () mutable
+        {
+            for(const auto& task: _tasks)
+                task.get();
+        }};
+}
 
 }
--- core/test/task_test.cpp
+++ core/test/task_test.cpp
@@ -1,6 +1,7 @@
 #include <taskex/core/taskex.h>
 
 #include <gtest/gtest.h>
+#include <atomic>
 
 //--------------------------------------------------
 using namespace taskex;
@@ -55,4 +56,65 @@ TEST_F(task_test, then_any_ret)
     ASSERT_STREQ("test", Task<int>([]{return 1;})
               .then<std::string>([](const auto&){ return "test";})
               .get().c_str());
+}
+
+TEST_F(task_test, CreateCollection_void)
+{
+    std::atomic<int> sum;
+    sum = 0;
+    {
+        CreateCollection<int>([&sum](int a){sum += a;}, {1,2,3,4,5});
+    }
+    ASSERT_EQ(sum, 15);
+}
+
+TEST_F(task_test, CreateCollection_notvoid)
+{
+    std::atomic<int> sum;
+    sum = 0;
+    {
+        CreateCollection<int, int>([&sum](int a){return sum += a;}, {1,2,3,4,5});
+    }
+    ASSERT_EQ(sum, 15);
+}
+
+TEST_F(task_test, ContainTasks_empty)
+{
+    ASSERT_NO_THROW(ContainTasks({}).get());
+}
+
+TEST_F(task_test, ContainTasks_void_notempty)
+{
+    int a1= 0;
+    int a2 = 0;
+    std::vector<Task<void>> tasks;
+    tasks.push_back(Task<void>([&a1](){a1 = 1;}));
+    tasks.push_back(Task<void>([&a2](){a2 = 2;}));
+    ContainTasks(std::move(tasks)).get();
+    ASSERT_EQ(a1, 1);
+    ASSERT_EQ(a2, 2);
+}
+TEST_F(task_test, ContainTasks_notvoid_notempty)
+{
+    int a1= 0;
+    int a2 = 0;
+    std::vector<Task<int>> tasks;
+    tasks.push_back(Task<int>([&a1](){return 1;}));
+    tasks.push_back(Task<int>([&a2](){return 2;}));
+    auto ret = ContainTasks(std::move(tasks)).get();
+    ASSERT_EQ(ret[0], 1);
+    ASSERT_EQ(ret[1], 2);
+}
+
+TEST_F(task_test, CreateCollection_ContainTasks)
+{
+    auto task = ContainTasks(CreateCollection<int, int>([](int a){ return a*a;}, {1,2,3,4,5}))
+                              .then<int>([](const auto& elements)
+                              {
+                                  int a = 0;
+                                  for(const auto& el:elements)
+                                      a += el;
+                                  return a;
+                              });
+    ASSERT_EQ(task.get(), 55);
 }
